959a422a2701a73cf43daf06c692ccf4f9beae70
AsilbekJr <89327572+AsilbekJr@users.noreply.github.com>
2022-12-08 14:59:58 +0500

    updated

--- src/components/Navbars/BotttomNavbar.jsx
+++ src/components/Navbars/BotttomNavbar.jsx
@@ -1,6 +1,6 @@
 import {  Box, Grid, useMediaQuery,  } from "@mui/material";
 import React, { useContext, useState } from "react";
-import { MyCustomTab } from "../../styles/Navbar";
+import { HomePage, MyCustomTab } from "../../styles/Navbar";
 import { useTheme } from '@mui/material/styles';
 import { MyContext } from "../../Context/Context";
 const BotttomNavbar = () => {
@@ -14,8 +14,9 @@ const BotttomNavbar = () => {
 
     matches &&   (<Box sx={{ background: "#333e48" }}>  
         <Grid container spacing={3} sx={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-          <Grid item >
-            <MyCustomTab onClick={() => setType("")}  label="Bosh sahifa" />
+          <Grid item >    
+              <HomePage onClick={() => setType("")} to="/">BOSH SAHIFA</HomePage>
+  
           </Grid>
           <Grid item >
             <MyCustomTab onClick={() => setType("Smartfonlar") }  label="Smartfonlar" />
